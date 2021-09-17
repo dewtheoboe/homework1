67725e3313b8a769a8cd424235a04d5fac8c9b28
Lauren Young <Lauren.Young@veteransunited.com>
2021-09-17 13:18:44 -0500

    Did all the homework for the first meeting

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        //Chapter 1 Q5
+
+        //static void Main(string[] args)
+        //{
+        //    System.Console.WriteLine("Good Day!");
+        //}
+
+        //Chapter 1 Q12
+
+        //.NET Framework includes C# as one component among many. It has libraries and Automated Memory Management and Common Language Runtime used for development.
+        //C# is specifically a programming language (general purpose high level development) 
+
+        //Chapter 2 Q8
+
+        //static void Main(string[] args)
+        //{
+        //    string hello = "Hello";
+        //    string world = "World";
+        //    object concat = hello + " " + world;
+        //    string together = concat.ToString();
+        //    System.Console.WriteLine(together);
+        //}
+
+        // Chapter 2 Q12
+
+        //static void Main(string[] args)
+        //{
+        //    string empFirstName;
+        //    string empLastName;
+        //    int empAge;
+        //    bool isManager;
+        //    uint empId;
+        //}
+
+        // Chapter 4 Q6
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter a number:");
+        //    int numA = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Enter another number:");
+        //    int numB = int.Parse(Console.ReadLine());
+
+        //    Console.WriteLine("The larger number is: " + Math.Max(numA, numB));
+        //}
+
+        // Chapter 4 Q7
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter a number:");
+        //    int numA; 
+        //    bool didItWork = int.TryParse(Console.ReadLine(), out numA);
+        //    while (didItWork == false)
+        //        {
+        //            Console.WriteLine("Enter a number:");
+        //            didItWork = int.TryParse(Console.ReadLine(), out numA);
+        //        }
+            
+        //    int numB;
+        //    Console.WriteLine("Enter another number:");
+        //    didItWork = int.TryParse(Console.ReadLine(), out numB);
+        //    while (didItWork == false)
+        //    {
+        //        Console.WriteLine("Enter another number:");
+        //        didItWork = int.TryParse(Console.ReadLine(), out numB);
+        //    }
+
+        //    int numC;
+        //    Console.WriteLine("Enter another number:");
+        //    didItWork = int.TryParse(Console.ReadLine(), out numC);
+        //    while (didItWork == false)
+        //    {
+        //        Console.WriteLine("Enter another number:");
+        //        didItWork = int.TryParse(Console.ReadLine(), out numC);
+        //    }
+
+        //    int numD;
+        //    Console.WriteLine("Enter another number:");
+        //    didItWork = int.TryParse(Console.ReadLine(), out numD);
+        //    while (didItWork == false)
+        //    {
+        //        Console.WriteLine("Enter another number:");
+        //        didItWork = int.TryParse(Console.ReadLine(), out numD);
+        //    }
+
+        //    int numE;
+        //    Console.WriteLine("Enter another number:");
+        //    didItWork = int.TryParse(Console.ReadLine(), out numE);
+        //    while (didItWork == false)
+        //    {
+        //        Console.WriteLine("Enter another number:");
+        //        didItWork = int.TryParse(Console.ReadLine(), out numE);
+        //    }
+
+        //    long totNum = (long) numA + numB + numC + numD + numE;
+        //    Console.WriteLine("The total is: " + totNum);
+        //}
+    }
+
+}
